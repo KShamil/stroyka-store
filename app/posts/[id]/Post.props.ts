1d8912497639598f91cc8447fd5dbdc99e18ec87
Shamil Kurbanov <107269680+KShamil@users.noreply.github.com>
2023-05-22 18:34:25 +0400

    update

--- app/posts/[id]/Post.props.ts
+++
@@ -1,7 +0,0 @@
-import { DetailedHTMLProps, HTMLAttributes } from 'react';
-
-export interface PostProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>,HTMLDivElement> {
-    params: {
-        id:string
-    }
-}
